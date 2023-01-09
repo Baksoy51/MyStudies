d22e78d0f5b176ac208268386baefb6d8e29b80d
bunyamin <aksoybunyamin@gmail.com>
2023-01-09 17:27:40 +0300

    third sennding

---
+++ src/ahmethocaPractise/C04_ForGithub.java
@@ -0,0 +1,8 @@
+package ahmethocaPractise;
+
+public class C04_ForGithub {
+    public static void main(String[] args) {
+        System.out.println("this class is made for only github ");
+
+    }
+}
